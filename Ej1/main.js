9fc41b556d1fd305158fd45a006c0815144256e8
danirus6 <44194174+danirus6@users.noreply.github.com>
2023-11-30 10:39:34 +0100

    tablas creadas

--- Ej1/main.js
+++ Ej1/main.js
@@ -8,7 +8,7 @@ app.use(express.json())
 
 const db = mysql.createConnection({
     host: 'localhost',
-    port: '6666',
+    port: '3306',
     user: 'root',
     password: 'Hellelo12343',
     database: 'expressSqlitoDB',
@@ -26,6 +26,29 @@ app.get('/createdb', (req, res) => {
         res.send('Database created...')
     })
 })
+
+app.get('/createCategories', (req, res) => {
+
+    const sql = 'CREATE TABLE categories(id INTEGER PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255) NOT NULL)';
+
+    db.query(sql,(err, result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send('Categories created...');
+    });
+});
+
+app.get('/createProducts', (req, res) => {
+
+    const sql = 'CREATE TABLE products(id INTEGER PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255) NOT NULL, price INT NOT NULL,category_id INT, FOREIGN KEY (category_id) REFERENCES categories (id) ON DELETE CASCADE ON UPDATE CASCADE)';
+
+    db.query(sql,(err, result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send('Products created...');
+    });
+});
+
 
 
 app.listen(PORT, () =>
