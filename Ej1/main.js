6d8d3bd832a60b627cddcf4a88ed723447a134b9
danirus6 <44194174+danirus6@users.noreply.github.com>
2023-12-01 12:50:43 +0100

    done all

--- Ej1/main.js
+++ Ej1/main.js
@@ -17,6 +17,38 @@ const db = mysql.createConnection({
 
 db.connect()
 
+app.get('/products', (req, res) => {
+    const sql = 'SELECT * FROM products'
+
+    db.query(sql, (err, result) => {
+        if (err) throw err
+        res.send({ message: 'Get products', result })
+    })
+});
+
+app.get('/categories', (req, res) => {
+    const sql = 'SELECT * FROM categories'
+
+    db.query(sql, (err, result) => {
+        if (err) throw err
+        res.send({ message: 'Get category', result })
+    })
+});
+
+app.get('/productosConCategorias', (req, res) => {
+  const sql = `
+    SELECT products.*, categories.name AS category_name
+    FROM products
+    INNER JOIN categories ON products.category_id = categories.id
+  `;
+
+    db.query(sql, (err, result) => {
+        if (err) throw err
+            console.log(result)
+            res.send('gg')
+    })
+});
+
 app.get('/createdb', (req, res) => {
     const sql = 'CREATE DATABASE expressSqlitoDB'
 
@@ -25,8 +57,43 @@ app.get('/createdb', (req, res) => {
         console.log(result)
         res.send('Database created...')
     })
-})
+});
 
+app.get('/getProduct/id/:id', (req, res) => {
+    const sql = `SELECT * FROM products WHERE id = ${req.params.id}`;
+    db.query(sql, (err, result) => {
+        if (err) throw err
+        console.log(result)
+        res.send('done')
+    })
+});
+app.get('/getProduct/name/:name', (req, res) => {
+    const productName = req.params.name;
+    const sql = `SELECT * FROM products WHERE name LIKE '%${productName}%'`;
+    db.query(sql, (err, result) => {
+        if (err) throw err
+        console.log(result)
+        res.send('done')
+    })
+});
+
+app.get('/getProductsDesc', (req, res) => {
+    const sql = `SELECT * FROM products ORDER BY id DESC`;
+    db.query(sql, (err, result) => {
+        if (err) throw err
+        console.log(result)
+        res.send('done')
+    })
+});
+
+app.get('/getCategory/id/:id', (req, res) => {
+    const sql = `SELECT * FROM categories WHERE id = ${req.params.id}`;
+    db.query(sql, (err, result) => {
+        if (err) throw err
+        console.log(result)
+        res.send('done')
+    })
+});
 app.get('/createCategories', (req, res) => {
 
     const sql = 'CREATE TABLE categories(id INTEGER PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255) NOT NULL)';
@@ -49,15 +116,84 @@ app.get('/createProducts', (req, res) => {
     });
 });
 
-// app.post('/createProduct', (req, res) => {
-//     const sql = 'INSERT INTO products (id, name, price, category_id
-
-// });
+app.post('/createProduct', (req, res) => {
+    const sql = `INSERT INTO products (name, price, category_id) VALUES ('${req.body.name}','${req.body.price}', '${req.body.category_id}');`
+    db.query(sql, (err, result) => {
+        if (err) throw err
+        console.log(result)
+        res.send('Product added...')
+    })
+});
 
 app.post('/createCategory', (req,res) =>{
     const sql = `INSERT INTO categories (name) values ('${req.body.name}');`;
 
+    db.query(sql, (err, result) => {
+        if (err) throw err
+        console.log(result)
+        res.send('Category added...')
+    })
 });
+
+//UPDATES
+app.put('/category/id/:id', (req, res) =>{
+    if(req.body.name){
+        const sql = `UPDATE categories SET name = '${req.body.name}' WHERE id = ${req.params.id}`;
+    }
+    db.query(sql, (err, result) => {
+        if (err) throw err
+        res.send('Category updated...')
+    })
+});
+
+app.put('/product/id/:id', (req, res) =>{
+
+    const productId = req.query.id;
+    const newName = req.query.name;
+    const newPrice = req.query.price;
+    const newCategoryId = req.query.category_id;
+
+    let updateQuery = 'UPDATE products SET ';
+    const updateValues = [];
+
+    if (newName) {
+        updateQuery += 'name = ?, ';
+        updateValues.push(newName);
+    }
+
+    if (newPrice) {
+        updateQuery += 'price = ?, ';
+        updateValues.push(newPrice);
+    }
+
+    if (newCategoryId) {
+        updateQuery += 'category_id = ?, ';
+        updateValues.push(newCategoryId);
+    }
+
+    updateQuery = updateQuery.slice(0, -2) + ' WHERE id = ?';
+    updateValues.push(productId);
+    
+    db.query(updateQuery, updateValues, (error, results) => {
+        if (error) {
+            res.status(500).json({ error: 'Error' });
+        } else {
+            res.json({ message: 'done' });
+        }
+    });
+});
+
+//DELETE
+app.delete('/deleteProduct/:id', (req, res) => {
+    const sql = `DELETE FROM products WHERE id = ${req.params.id}`
+
+    db.query(sql, (err, result) => {
+        if (err) throw err
+        res.send('Post deleted')
+    })
+})
+
+
 
 app.listen(PORT, () =>
     console.log(`This server is started at port http://localhost:${PORT}`)
