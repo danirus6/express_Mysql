ff2839a6517f27921b0883495fbb625de6e10b34
danirus6 <44194174+danirus6@users.noreply.github.com>
2023-12-01 18:10:36 +0100

    stash

--- Ej1/main.js
+++ Ej1/main.js
@@ -4,15 +4,14 @@ const mysql = require('mysql2')
 
 const PORT = 3000;
 
-app.use(express.json())
+app.use(express.json());
 
 const db = mysql.createConnection({
     host: 'localhost',
     port: '3306',
     user: 'root',
     password: 'Hellelo12343',
-    database: 'expressSqlitoDB',
-
+    database: 'expressSqlitoDB', //esto comentar la primera vez para crear la database
 });
 
 db.connect()
