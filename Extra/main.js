d53dd1a9c7f1a55e63d6fe47cb4c7998c278b925
danirus6 <44194174+danirus6@users.noreply.github.com>
2023-12-02 13:50:06 +0100

    update

--- Extra/main.js
+++ Extra/main.js
@@ -16,8 +16,29 @@ const db = mysql.createConnection({
 
 db.connect()
 
+//Añadir Usuarios
+
+app.post('/createUser', (req, res) => {
+    const sql = `INSERT INTO users (name, order_id) VALUES ('${req.body.name}', '${req.body.orders_id}');`
+    db.query(sql, (err, result) => {
+        if (err) throw err
+        console.log(result)
+        res.send('User added...')
+    })
+});
+
+//Añadir pedidos
 
 
+//SELECT ALL USERS
+
+//SELECT ALL ORDERS
+
+//GET USERS_ORDERS
+
+//GET BY USER:ID
+
+//DELETE USERBYID
 
 app.listen(PORT, () =>
     console.log(`This server is started at port http://localhost:${PORT}`)
